db57e3ef690d4f8081ddac3a65261ef35823a22c
Nimisha Yadav <iamnimishayadav@gmail.com>
2022-04-30 22:55:06 +0530

    added remote sql

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,9 +1,19 @@
 <?php
 //SCRIPT TO CONNECT TO THE DATABASE
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "forum";
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$database = "forum";
+//$conn = mysqli_connect($servername, $username, $password, $database);
+
+//SCRIPT TO REMOTE SQL
+$servername = "remotemysql.com";
+$username = "bDKXdhXxVW";
+$password = "nBj4binYuW";
+$database = "bDKXdhXxVW";
 $conn = mysqli_connect($servername, $username, $password, $database);
+
+
+
 
 ?>
