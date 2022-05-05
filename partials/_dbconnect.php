5c8e9dd405547a5b4a8bc6b54d3da02cd0a10d49
Nimisha Yadav <iamnimishayadav@gmail.com>
2022-05-05 20:20:51 +0530

    Update _dbconnect.php

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,19 +1,8 @@
 <?php
 //SCRIPT TO CONNECT TO THE DATABASE
-//$servername = "localhost";
-//$username = "root";
-//$password = "";
-//$database = "forum";
-//$conn = mysqli_connect($servername, $username, $password, $database);
-
-//SCRIPT TO REMOTE SQL
-$servername = "remotemysql.com";
-$username = "bDKXdhXxVW";
-$password = "nBj4binYuW";
-$database = "bDKXdhXxVW";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "forum";
 $conn = mysqli_connect($servername, $username, $password, $database);
-
-
-
-
 ?>
