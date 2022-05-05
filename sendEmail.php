209ec278f4313c4abb49086d481cc985fef04a2c
Nimisha Yadav <iamnimishayadav@gmail.com>
2022-05-05 20:22:08 +0530

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -18,7 +18,7 @@ if(isset($_POST['name']) && isset($_POST['email'])){
     $mail->Host = "smtp.gmail.com";
     $mail->SMTPAuth = true;
     $mail->Username = "osvisstudio@gmail.com";
-    $mail->Password = 'oshere311!';
+    $mail->Password = '';
     $mail->Port = 465;
     $mail->SMTPSecure = "ssl";
 
